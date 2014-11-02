0b6bcb1962c9d22fef9a026df9f4231c7d27e6ed
Pavel Lomonosov <pavel.lomonosov@sap.com>
2014-11-02 17:11:00 +0200

    init loader

--- client/questions/questions.controller.js
+++
@@ -1,9 +0,0 @@
-sap.ui.controller("sap.als.questions", {
-
-    onInit: function () {
-
-        var questionsModel = new sap.ui.model.json.JSONModel();
-
-        this.getView().setModel(questionsModel, "questionsModel");
-    }
-});
