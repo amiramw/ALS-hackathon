825f4adc7aea884de19398cd7e4ced8aaad14d60
Isaac Eliassi <isaac.eliassi@sap.com>
2014-11-09 18:24:49 +0200

    Change the id datatype from binary to big int

--- ALS_backend/src/main/resources/DB_CERATE.sql
+++ ALS_backend/src/main/resources/DB_CERATE.sql
@@ -1,17 +1,12 @@
+-- Drop the schema in order to create a clean one
+DROP SCHEMA IF EXISTS db_ALS;
+
 -- The main schema creation
 CREATE SCHEMA IF NOT EXISTS db_ALS;
 
-insert into tbl_patient (id) values (uuid());
-
 -- The table with all the ALS positive
-DROP table if exists tbl_form_ext; 
-DROP table if exists tbl_form;
-DROP table if exists tbl_sensor_ext;
-DROP table if exists tbl_sensor;
-DROP TABLE if exists tbl_patient;
-
 CREATE TABLE IF NOT EXISTS`db_als`.`tbl_patient` (
-  `id` VARCHAR(36) NOT NULL,
+  `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
   `email` VARCHAR(225) NULL,
   `LastName` VARCHAR(45) NULL,
   `FirstName` VARCHAR(45) NULL,
@@ -24,7 +19,7 @@ CREATE TABLE IF NOT EXISTS`db_als`.`tbl_patient` (
   
 -- The table with all the result of the submitted forms
 CREATE TABLE IF NOT EXISTS`db_als`.`tbl_form` (
-  `id` VARCHAR(36) NOT NULL,
+  `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
   `SubmitionDate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `Q1` TINYINT NULL,
   `Q2` TINYINT NULL,
@@ -38,7 +33,7 @@ CREATE TABLE IF NOT EXISTS`db_als`.`tbl_form` (
   `Q10` TINYINT NULL,
   `Q11` TINYINT NULL,
   `Q12` TINYINT NULL,
-  `EXT_ID` VARCHAR(36),
+  `EXT_ID` BIGINT UNSIGNED NOT NULL,
   UNIQUE INDEX `EXT_ID_UNIQUE` (`EXT_ID` ASC),
   CONSTRAINT `fk_id_id`
     FOREIGN KEY (`id`)
@@ -48,11 +43,11 @@ CREATE TABLE IF NOT EXISTS`db_als`.`tbl_form` (
 
 -- The table with the data from the sensors (equivalent to the form)
 CREATE TABLE IF NOT EXISTS`db_als`.`tbl_sensor` (
-  `id` VARCHAR(36) NOT NULL,
+  `id` BIGINT UNSIGNED NOT NULL,
   `SubmitionDate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `SensorType` CHAR NOT NULL,
   `Data` BLOB NOT NULL,
-  `EXT_ID` VARCHAR(36),
+  `EXT_ID` BIGINT UNSIGNED NOT NULL,
   UNIQUE INDEX `EXT_ID_UNIQUE` (`EXT_ID` ASC),
   CONSTRAINT `fk_id_id_sensor`
     FOREIGN KEY (`id`)
@@ -62,7 +57,7 @@ CREATE TABLE IF NOT EXISTS`db_als`.`tbl_sensor` (
 
 -- The tables to add additional data about a specific sensor/form row
 CREATE TABLE IF NOT EXISTS`db_als`.`tbl_form_ext` (
-  `id` VARCHAR(36) NOT NULL,
+  `id` BIGINT UNSIGNED NOT NULL,
   `Key` VARCHAR(225) NULL,
   `Val` VARCHAR(1000) NULL,
   INDEX `fk_ext_id_form_idx_idx` (`id` ASC),
@@ -73,7 +68,7 @@ CREATE TABLE IF NOT EXISTS`db_als`.`tbl_form_ext` (
     ON UPDATE NO ACTION);
 
 CREATE TABLE IF NOT EXISTS`db_als`.`tbl_sensor_ext` (
-  `id` VARCHAR(36) NOT NULL,
+  `id` BIGINT UNSIGNED NOT NULL,
   `Key` VARCHAR(225) NULL,
   `Val` VARCHAR(1000) NULL,
   INDEX `fk_ext_id_sensor_idx` (`id` ASC),
