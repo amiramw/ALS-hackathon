34ff28292bace46dd80abc0006521dc102e4a9bf
Shachar Witkovsky <shachar.witkovsky@sap.com>
2014-11-09 18:27:50 +0200

    connect questions to model

--- client/view/questions.view.js
+++ client/view/questions.view.js
@@ -14,6 +14,7 @@ sap.ui.jsview("view.questions", {
             path: "/questions",
             factory: function(sId, oContext) {
                 var questions = oContext.getModel().getData().questions;
+
                 var answers = oContext.getProperty("answers") || [];
                 var index = parseInt(oContext.getPath().replace("/questions/",""), 10);
                 var nextIndex = 0;
@@ -23,14 +24,48 @@ sap.ui.jsview("view.questions", {
                     nextIndex = index + 1;
                 }
 
-
-                var page = new sap.m.Page("page_" + oContext.getProperty("id"),{title:"{title}"});
+                var pageId = oContext.getProperty("id");
+                var page = new sap.m.Page("page_" + pageId,{title:"{title}"});
 
                 var box = new sap.m.VBox();
 
                 for (i = 0; i < answers.length; i++) {
                     box.addItem(new sap.m.RadioButton({
-                        text: answers[i]
+                        text: answers[i].title,
+                        customData: [new sap.ui.core.CustomData({
+                            key: 'answerId',
+                            value: answers[i].id
+                        }),
+                        new sap.ui.core.CustomData({
+                            key: 'pageId',
+                            value: pageId
+                        })]
+                            ,
+                        select: function(e){
+                            var data = this.getCustomData();
+                            var answer = {};
+                            var i;
+
+                            for (i = 0; i < data.length; i++) {
+                                answer[data[i].getKey()] = data[i].getValue();
+                            }
+
+                            var res = this.getModel("resModel").getData();
+                            var found = false;
+                            for (i = 0; i < res.questionsRes.length; i++) {
+                                if (res.questionsRes[i].pageId === answer.pageId) {
+                                    found = true;
+                                    res.questionsRes[i].answerId = answer.answerId;
+                                    break;
+                                }
+                            }
+
+                            if (!found) {
+                                res.questionsRes.push(answer);
+                            }
+
+                            this.getModel("resModel").setData(res);
+                        }
                     }));
                 }
 
@@ -47,8 +82,8 @@ sap.ui.jsview("view.questions", {
     },
 
     onAfterRendering: function(){
-        //var data = this.getModel().oData;
-        //console.log(data);
+        this.resModel = new sap.ui.model.json.JSONModel({questionsRes: []});
+        this.setModel(this.resModel, "resModel");
     }
 
 });
