a41f72420d47bd57b7751c456263b7ac371aa5f4
Guy Roth <guy.roth@sap.com>
2014-11-10 19:40:25 +0200

    Remove old client directory

--- client/view/questions.controller.js
+++
@@ -1,9 +0,0 @@
-sap.ui.controller("view.questions", {
-
-    onInit: function () {
-
-        var questionsModel = new sap.ui.model.json.JSONModel("data/questions.json");
-
-        this.getView().setModel(questionsModel);
-    }
-});
