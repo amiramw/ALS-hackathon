4ce48cc9989437436299382c0bd74d648aa917ec
Amiram Wingarten <amiram.wingarten@sap.com>
2014-11-16 16:04:20 +0200

    add cross origin filter in order to allow ui calls from other domains

---
+++ ALS_backend/src/main/java/als/api/servlets/CrossOriginFilter.java
@@ -0,0 +1,39 @@
+package als.api.servlets;
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * filter for testing purposes to allow cross origin calls between ui and backend
+ * @author I066470
+ *
+ */
+public class CrossOriginFilter implements Filter {
+
+	@Override
+	public void destroy() {
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res,
+			FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+		response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, HEAD");
+        response.addHeader("Access-Control-Allow-Headers", "X-PINGOTHER, Origin, X-Requested-With, Content-Type, Accept");
+        response.addHeader("Access-Control-Max-Age", "1728000");
+        
+        chain.doFilter(req, res);
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+	}
+
+}
